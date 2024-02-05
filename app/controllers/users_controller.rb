dd24e514e1eef89b8857b40f6ba20768cd22e77b
Kevinkp09 <kevinpatel907.kp@gmail.com>
2024-02-05 11:32:53 +0530

    Adding sessions for user so that the user stays logged in and adding helper methods to authenticate that

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,10 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: [:edit, :update, :show]
   def index
     @users = User.paginate(page: params[:page], per_page: 3)
   end
 
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 2)
   end
 
@@ -16,6 +15,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to Alpha Blog #{@user.username}. You have succesfully signed up"
       redirect_to @user
     else
@@ -24,11 +24,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully updated"
       redirect_to articles_path
@@ -40,5 +38,9 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   resources :articles
   get "signup", to: "users#new"
   resources :users, except: [:new]
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get "/login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
 end
